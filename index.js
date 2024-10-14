3fbefbcb705826f583f78e81636e7b3915b6ccdb
Gaffar Adif <gaffar.adif@gmail.com>
2024-10-14 16:17:39 +0600

    remove web teligram things

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv'); // Correct way to import dotenv
 const cors = require('cors'); // Import cors package
 const telegramRoutes = require('./Routes/teligramRoute');
 const errorHandler = require('./MIddleware/errorMiddleware');
-const axios = require('axios');
 const { startBot } = require('./bot'); // Import the startBot function
 
 // Load environment variables from the .env file
@@ -23,17 +22,6 @@ app.use('/telegram', telegramRoutes); // Telegram routes
 // Error handling middleware
 app.use(errorHandler);
 
-// Set up Telegram webhook
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/setWebhook?url=https://t.me/adifdemobot_bot/adufadfkhdf`);
-        res.send(response.data);
-        console.log('Teligram info', response.data);
-    } catch (error) {
-        console.error('Error setting webhook:', error);
-        res.status(500).send('Failed to set webhook');
-    }
-});
 
 // Start the Telegram bot and listen for incoming requests
 app.listen(port, () => {
